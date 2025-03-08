3db423b2a22ed45db9cdfcd6c587c031a6049814
Onran0 <kotshel2009@gmail.com>
2025-03-08 22:04:04 +0900

    wrapper version changed to 1.0-A

--- wrapper/src/main/java/io/github/spookylauncher/wrapper/SLWrapper.java
+++ wrapper/src/main/java/io/github/spookylauncher/wrapper/SLWrapper.java
@@ -15,7 +15,7 @@ import java.io.File;
 import java.util.*;
 
 public final class SLWrapper implements Runnable {
-    public static final String VERSION = "1.0-B";
+    public static final String VERSION = "1.0-A";
     public static final String LOG_ID = "spookylaunch wrapper " + VERSION;
 
     private final Properties config;
