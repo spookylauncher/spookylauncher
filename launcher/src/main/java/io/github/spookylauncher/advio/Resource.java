755295c91394a2926756a7e3b22748a525444bd8
Onran0 <kotshel2009@gmail.com>
2025-10-29 17:38:00 +0900

    enhanced exception handle in Resource

--- launcher/src/main/java/io/github/spookylauncher/advio/Resource.java
+++ launcher/src/main/java/io/github/spookylauncher/advio/Resource.java
@@ -1,15 +1,22 @@
 package io.github.spookylauncher.advio;
 
+import io.github.spookylauncher.log.Level;
+import io.github.spookylauncher.log.Logger;
+
 import java.io.*;
 
 public final class Resource {
+    private static final String LOG_ID = "resources provider";
+
     public static boolean exists(String resource) {
         try {
             try(final InputStream is = Resource.class.getResourceAsStream("/" + resource)) {
                 if(is != null)
                     return true;
             }
-        } catch(final IOException e) { e.printStackTrace(); }
+        } catch(final IOException e) {
+            Logger.log(Level.ERROR, LOG_ID, e);
+        }
 
         return false;
     }
