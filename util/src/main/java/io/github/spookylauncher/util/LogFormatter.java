2c2a144974c7c4ede3c15026e03968ce242020c9
Onran0 <kotshel2009@gmail.com>
2025-12-09 18:22:22 +0900

    now LogFormatter also formats level name

--- util/src/main/java/io/github/spookylauncher/util/LogFormatter.java
+++ util/src/main/java/io/github/spookylauncher/util/LogFormatter.java
@@ -10,12 +10,13 @@ import java.time.format.DateTimeFormatter;
 public class LogFormatter extends Formatter {
 
     private static final int LOGGER_FIELD_LENGTH = 25;
+    private static final int LEVEL_FIELD_LENGTH = 6;
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss.SSSZ");
 
-    private String formatLoggerName(String loggerName) {
+    private String formatField(int length, String loggerName) {
         StringBuilder sb = new StringBuilder();
 
-        int spacesCount = LOGGER_FIELD_LENGTH - loggerName.length();
+        int spacesCount = length - loggerName.length();
 
         for(int i = 0; i < spacesCount; i++)
             sb.append(" ");
@@ -27,12 +28,19 @@ public class LogFormatter extends Formatter {
 
     @Override
     public String format(LogRecord record) {
-        return "[" + record.getLevel() + "] " +
+        String res = "[" + formatField(LEVEL_FIELD_LENGTH, record.getLevel().getName()) + "] " +
                 Instant.ofEpochMilli(record.getMillis())
                         .atZone(ZoneId.of("Europe/Moscow"))
                         .format(formatter) +
-                " [" + formatLoggerName(record.getLoggerName()) + "] " +
-                formatMessage(record) +
-                System.lineSeparator();
+                " [" + formatField(LOGGER_FIELD_LENGTH, record.getLoggerName()) + "] " +
+                formatMessage(record);
+
+        if(record.getThrown() != null) {
+            res += " : " + record.getSourceClassName() + "." +
+                    record.getSourceMethodName() + "(): " +
+                    record.getThrown().toString();
+        }
+
+        return res + System.lineSeparator();
     }
 }
