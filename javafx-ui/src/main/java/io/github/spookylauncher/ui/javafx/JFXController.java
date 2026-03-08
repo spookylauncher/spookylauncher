11d23c3765a481ae23fd07ebd4c5957071f59bd6
Onran <kotshel2009@gmail.com>
2026-03-08 22:56:48 +0900

    added preview resizing via jfx controller

--- javafx-ui/src/main/java/io/github/spookylauncher/ui/javafx/JFXController.java
+++ javafx-ui/src/main/java/io/github/spookylauncher/ui/javafx/JFXController.java
@@ -3,14 +3,24 @@ package io.github.spookylauncher.ui.javafx;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 
 public class JFXController {
 
     public Button articleButton, playButton, versionsButton, settingsButton;
+    public ImageView versionPreview;
+    public Pane versionPreviewPane;
     public Parent root;
 
     public void initialize() {
         applySettings(root);
+
+        versionPreview.fitWidthProperty().bind(versionPreviewPane.widthProperty());
+        versionPreview.fitHeightProperty().bind(versionPreviewPane.heightProperty());
+
+        versionPreview.setSmooth(true);
+        versionPreview.setManaged(false);
     }
 
     private void applySettings(Parent root) {
